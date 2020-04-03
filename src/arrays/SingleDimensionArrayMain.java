531f7028d68cbcf7b98bc48393dff81300f1313b
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-03 23:53:27 +0530

    creating an object of SingleDimensionArray class

--- src/arrays/SingleDimensionArrayMain.java
+++ src/arrays/SingleDimensionArrayMain.java
@@ -2,6 +2,7 @@ package arrays;
 // here we will call all the methods declared in SingleDimensionalArray
 public class SingleDimensionArrayMain {
     public static void main(String[] args) {
-
+        System.out.println("Creating a block array of size 10");
+        SingleDimensionalArray sda = new SingleDimensionalArray(10);
     }
 }
