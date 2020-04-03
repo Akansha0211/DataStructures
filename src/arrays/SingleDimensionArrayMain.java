e648b7a29d4cf03ecd919123e0e57e07820aa821
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-03 23:37:35 +0530

    create class SingleDimensionalArray

---
+++ src/arrays/SingleDimensionArrayMain.java
@@ -0,0 +1,7 @@
+package arrays;
+// here we will call all the methods declared in SingleDimensionalArray
+public class SingleDimensionArrayMain {
+    public static void main(String[] args) {
+
+    }
+}
