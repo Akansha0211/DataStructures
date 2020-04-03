257143693e2a728e26c5fc91b1ac66eaae8983b5
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-03 13:30:08 +0530

    call a static method i.e factorial() from another class ie Factorial

--- src/Recursion/MainForFactorial.java
+++ src/Recursion/MainForFactorial.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class MainForFactorial {
     private static Scanner scan = new Scanner(System.in);
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+        System.out.println("Enter an integer");
+        int x = scan.nextInt();
+        Factorial.factorial(x);
     }
 }
