2e6af4e982fc54ddb22acac6af89590451f8905b
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-03 13:27:00 +0530

    create Scanner object

---
+++ src/Recursion/MainForFactorial.java
@@ -0,0 +1,10 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class MainForFactorial {
+    private static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+}
