799c0412dbe58d044925ef4ba38358feea2a7307
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-03 23:50:05 +0530

    create a constructor

--- src/arrays/SingleDimensionalArray.java
+++ src/arrays/SingleDimensionalArray.java
@@ -1,5 +1,13 @@
 package arrays;
-
+//here we will write all the operations ior methods that can be performed on the array
 public class SingleDimensionalArray {
+    int[] arr = null;
 
+    // for creating a blank array create a constructor ad pass size of array as an argument
+    public SingleDimensionalArray(int sizeOfArray) {
+        arr = new int[sizeOfArray];
+        for (int i=0; i<arr.length;i++){
+            arr[i] = Integer.MIN_VALUE;
+        }
+    }
 }
