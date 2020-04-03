614579ef03ff57f273440d63ba6b42c963eff9e3
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-04 00:26:06 +0530

    inserting elements in Array by calling insert method

--- src/arrays/SingleDimensionArrayMain.java
+++ src/arrays/SingleDimensionArrayMain.java
@@ -8,5 +8,18 @@ public class SingleDimensionArrayMain {
         System.out.println("Printing the Array");
         sda.traverseArray();
 
+        System.out.println("Inserting the value in the array...");
+        sda.insert(1,0);
+        sda.insert(1,10);
+        sda.insert(2,20);
+        sda.insert(3,30);
+        sda.insert(4,40);
+        sda.insert(5,50);
+        sda.insert(6,60);
+        sda.insert(7,70);
+        sda.insert(8,80);
+        sda.insert(1,100);
+        sda.insert(12,120);
+
     }
 }
