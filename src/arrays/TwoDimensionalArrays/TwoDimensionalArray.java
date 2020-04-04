b6925b9268ce8af6f205677e16badd61ad44288c
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-04 17:00:13 +0530

    create insertValueInArray() method

--- src/arrays/TwoDimensionalArrays/TwoDimensionalArray.java
+++ src/arrays/TwoDimensionalArrays/TwoDimensionalArray.java
@@ -12,6 +12,10 @@ public class TwoDimensionalArray {
             }
         }
     }
+
+    /**
+     * This method visits all the elements of the array
+     */
     public void traverseArray(){
         try{
             for (int rows = 0; rows <arr.length ; rows++) {
@@ -22,6 +26,26 @@ public class TwoDimensionalArray {
         }
         catch(Exception e){
             System.out.println("Array does not exist");
+        }
+    }
+
+    /**
+     * This method wil insert a value at any particular index of the array
+     * @param rows number of rows in the array
+     * @param col number of columns in the array
+     * @param value value which is to inserted in the array
+     */
+    public void insertValueInArray(int rows, int col, int value){
+        try{
+            if(arr[rows][col]==Integer.MIN_VALUE){
+                arr[rows][col] = value;
+            }
+            else{
+                System.out.println("This cell is already occupied");
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid index to insert a value");
         }
     }
 
