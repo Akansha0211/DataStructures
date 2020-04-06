d73490be0a6d16ffb1733c08cc340900848f45a9
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-07 00:26:59 +0530

    create Data Fields of integer type

--- src/LinkedList/SingleLL/Node.java
+++ src/LinkedList/SingleLL/Node.java
@@ -1,4 +1,14 @@
 package LinkedList.SingleLL;
 
+/**
+ *   A node is a Data Structure that contains data and one or more links.
+ *   A link is a reference to a node
+ *   Node can be of type Generic and type can be specified at the time of object creation
+ *   But here we are not creating Generic type Node...
+ */
 public class Node {
+    //Data Fields
+    private int data;
+    private Node next;
+
 }
