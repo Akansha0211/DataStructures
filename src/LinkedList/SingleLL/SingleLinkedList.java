3f50675bebe4204f135d4c4b4762410be8926937
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-07 02:13:24 +0530

    create insertLinkedList() method

--- src/LinkedList/SingleLL/SingleLinkedList.java
+++ src/LinkedList/SingleLL/SingleLinkedList.java
@@ -22,6 +22,20 @@ public class SingleLinkedList {
     public void setTail(Node tail){
         this.tail = tail;
     }
+
+    /**
+     * This method will create a Single linkedList
+     *
+     * steps:
+     * step1: create a head refernce and tail refenenec and assign them with blank values
+     * as till now it the List is empty.
+     * step 2: create a node and give data to it and se its next to Null
+     * step 3: Now set the head reference to Node which you have created
+     * and the tail will also point to that Node as you are having only one Node in the linkedList...
+     *
+     * @param nodeValue it is data in the Node
+     * @return head  as we need to know the first reference of the Node i.e location of first node in the memory...
+     */
     public Node createSingleLinkedList(int nodeValue){
         head = new Node();
         Node node = new Node();
@@ -31,7 +45,10 @@ public class SingleLinkedList {
         tail = node;
         size = 1;
         return head;
-
+    }
+    public void insertLinkedList(int nodeValue,int location){
+        Node node = new Node();
+        node.setData(nodeValue);
 
     }
 
