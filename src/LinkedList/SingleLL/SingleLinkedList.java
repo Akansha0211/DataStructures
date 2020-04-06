1c1abc09ac4967837335c9018da0b9f45a8d7798
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-07 01:09:28 +0530

    create data fields for the LnkedList

---
+++ src/LinkedList/SingleLL/SingleLinkedList.java
@@ -0,0 +1,7 @@
+package LinkedList.SingleLL;
+
+public class SingleLinkedList {
+    private Node head; // stores reference of the first node i.e where the first node is stored in the memory
+    private Node tail ; // stores the refernce of the last node .This reference is basically used to reduce time complexity
+    private int size; // stores the current size of the Linked Lit
+}
