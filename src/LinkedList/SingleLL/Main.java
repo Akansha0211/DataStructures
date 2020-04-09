98fdcc8260306b9907f2cf5771b69edc02812d7b
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-09 18:50:58 +0530

    call createSingleLinkedList() , traverseLinkedList() , insertLinkedList() methods in main() method of Main class

--- src/LinkedList/SingleLL/Main.java
+++ src/LinkedList/SingleLL/Main.java
@@ -3,6 +3,13 @@ package LinkedList.SingleLL;
 public class Main {
     public static void main(String[] args) {
         SingleLinkedList list = new SingleLinkedList();
-
+        list.createSingleLinkedList(5);
+        list.traverseLinkedList();
+        list.insertLinkedList(20,2);
+        list.traverseLinkedList();
+        list.insertLinkedList(25,3);
+        list.insertLinkedList(30,4);
+        list.insertLinkedList(35,5);
+        list.traverseLinkedList();
     }
 }
