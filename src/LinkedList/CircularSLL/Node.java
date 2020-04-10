8405e2673aa28dcd48806975378012ff909fae67
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-10 23:56:56 +0530

    create a constructor for Node class

--- src/LinkedList/CircularSLL/Node.java
+++ src/LinkedList/CircularSLL/Node.java
@@ -4,5 +4,14 @@ public class Node {
     int data;
     Node next;
 
+    /**
+     * Create a constructor to create a blank node i.e when the first node is added in the Linked List...
+     * @param dataItem
+     */
+    public Node(int dataItem){
+        dataItem = data;
+        next = null;
+    }
+
 
 }
