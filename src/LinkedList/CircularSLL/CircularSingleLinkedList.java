0d99cab213dd58e58cebdc7493b09521e82237ae
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-12 15:52:31 +0530

    create getters and setters for the CircularSingleLinkedList class

--- src/LinkedList/CircularSLL/CircularSingleLinkedList.java
+++ src/LinkedList/CircularSLL/CircularSingleLinkedList.java
@@ -5,5 +5,24 @@ public class CircularSingleLinkedList {
     private  Node tail;
     private int size;
 
+    public Node getHead(){
+        return head;
+    }
+    public Node getTail(){
+        return tail;
+    }
+    public int getSize(){
+        return size;
+    }
+    public void setHead(Node head){
+        this.head = head;
+    }
+    public void setTail(Node tail){
+        this.tail = tail;
+    }
+    public void setSize(int size){
+        this.size = size;
+    }
+
 
 }
