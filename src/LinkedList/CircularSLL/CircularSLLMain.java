467b818cbdcc67ea8e66e824639ba54df458b496
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-12 17:58:45 +0530

    create object of CircularSingleLinkedList class in main method

--- src/LinkedList/CircularSLL/CircularSLLMain.java
+++ src/LinkedList/CircularSLL/CircularSLLMain.java
@@ -1,5 +1,8 @@
 package LinkedList.CircularSLL;
 
 public class CircularSLLMain {
+    public static void main(String[] args) {
+        CircularSingleLinkedList list = new CircularSingleLinkedList();
 
+    }
 }
