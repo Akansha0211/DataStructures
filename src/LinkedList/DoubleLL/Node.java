6c62e9353383b7b54f372bca6ba01eda62b61d00
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-14 22:39:47 +0530

    create getters and setters for the Node class

--- src/LinkedList/DoubleLL/Node.java
+++ src/LinkedList/DoubleLL/Node.java
@@ -16,8 +16,25 @@ public class Node {
         next = this.next;
         prev = previous;
     }
+    public int getData(){
+        return data;
+    }
+    public Node getNext(){
+     return next;
+    }
+    public Node getPrevious(){
+        return previous;
+    }
 
+    public void setData(int data) {
+        this.data = data;
+    }
 
+    public void setNext(Node next) {
+        this.next = next;
+    }
 
-
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
 }
