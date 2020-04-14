ac3ab8c384b639e1289142635f62031211d17025
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-14 23:07:43 +0530

    create a method createLinkedList() method

--- src/LinkedList/DoubleLL/DoubleLinkedList.java
+++ src/LinkedList/DoubleLL/DoubleLinkedList.java
@@ -5,5 +5,21 @@ public class DoubleLinkedList {
     private Node tail;
     private int size;
 
-
+    /**
+     * This method will create a node
+     * @param nodeValue data value of the node
+     * @return head .i.e location of the 1st element...
+     */
+    public Node createLinkedList(int nodeValue){
+        head = new Node();
+        tail = new Node();
+        Node node = new Node();
+        node.setData(nodeValue);
+        head = node;
+        tail = node;
+        node.setNext(null);
+        node.setPrevious(null);
+        size = 1;
+        return head;
+    }
 }
