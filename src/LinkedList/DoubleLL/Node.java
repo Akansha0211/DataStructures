7879584ecdc39224ce8f9a70da7dc32f0e4f8992
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-14 22:36:00 +0530

    create a constructor of the Node class

--- src/LinkedList/DoubleLL/Node.java
+++ src/LinkedList/DoubleLL/Node.java
@@ -5,5 +5,19 @@ public class Node {
     private Node next;
     private Node previous;
 
+    /**
+     * This constructor will crate a node on creating an object of Node class
+     * @param dataItem data value of the node
+     * @param next stores the reference of the next node
+     * @param prev stores the reference of the previous node...
+     */
+    public Node(int dataItem, Node next, Node prev){
+        dataItem = data;
+        next = this.next;
+        prev = previous;
+    }
+
+
+
 
 }
