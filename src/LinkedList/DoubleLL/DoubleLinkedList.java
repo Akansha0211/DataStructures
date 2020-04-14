46ba16132133b5c97acc66859ba9d19321da4bbf
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-14 23:41:08 +0530

    create getters and setters of DoubleLinkedList class

--- src/LinkedList/DoubleLL/DoubleLinkedList.java
+++ src/LinkedList/DoubleLL/DoubleLinkedList.java
@@ -5,6 +5,30 @@ public class DoubleLinkedList {
     private Node tail;
     private int size;
 
+    public Node getHead() {
+        return head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void setTail(Node tail) {
+        this.tail = tail;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
     /**
      * This method will create a node
      * @param nodeValue data value of the node
@@ -18,8 +42,57 @@ public class DoubleLinkedList {
         head = node;
         tail = node;
         node.setNext(null);
-        node.setPrevious(null);
+        node.setPrevious(null); // this is difference from SingleLinkedList
         size = 1;
         return head;
     }
+
+    /**
+     * This method will return the value of head != null in boolean
+     * i.e it will return true if LinkedList exists otherwise false.
+     * @return boolean value...
+     */
+    public boolean existsLinkedList(){
+        return head !=null;
+    }
+
+    /**
+     *
+     * @param nodeValue
+     * @param location
+     */
+    public void insertInLinkedList(int nodeValue, int location){
+        Node node = new Node();
+        node.setData(nodeValue);
+        if (!existsLinkedList()){
+            System.out.println("LinkedList does not exists");
+            return;
+        }
+        else if (location == 0){
+            node.setPrevious(null);
+            node.setNext(head);
+            head.setPrevious(node);
+            head = node;
+        }
+        else if (location >= size){
+            node.setNext(null);
+            node.setPrevious(tail);
+            tail.setNext(node);
+            tail = node;
+        }
+        else {
+            Node tmpNode = head;
+            for (int index = 0; index <location-1; index++) {
+                tmpNode = tmpNode.getNext();
+            }
+            node.setPrevious(tmpNode);
+            node.setNext(tmpNode.getNext());
+            tmpNode.setNext(node);
+            node.getNext().setPrevious(node);
+        }
+        size++; // size increased as one node is added...
+    }
+
+
+
 }
