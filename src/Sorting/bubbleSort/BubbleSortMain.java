89035eafa8a46b96fd967c9cc88a4ed62d02d1d2
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-15 11:50:58 +0530

    create object of BubbleSort class and call its methods

--- src/Sorting/bubbleSort/BubbleSortMain.java
+++ src/Sorting/bubbleSort/BubbleSortMain.java
@@ -1,4 +1,15 @@
 package Sorting.bubbleSort;
 
 public class BubbleSortMain {
+    public static void main(String[] args) {
+        BubbleSort  bubsort = new BubbleSort();
+        int[] array ={17,23,10,14,2,1,8,0};
+        System.out.println("Array to be sorted ");
+        bubsort.printArray(array);
+
+        System.out.println("Sorted Array...");
+        bubsort.bubbleSort(array);
+        bubsort.printArray(array);
+
+    }
 }
