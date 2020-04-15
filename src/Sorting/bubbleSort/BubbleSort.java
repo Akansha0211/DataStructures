af938adb8187c9e245514f58792b0f4dbc314d71
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-15 12:55:39 +0530

    changes in bubbleSort() method

--- src/Sorting/bubbleSort/BubbleSort.java
+++ src/Sorting/bubbleSort/BubbleSort.java
@@ -15,7 +15,7 @@ public class BubbleSort {
         for (int i = 0; i <n-1 ; i++) {
             for (int j = 0; j < n-i-1; j++) {
                 if (array[j]>array[j+1]){
-                    int temp = array[i];
+                    int temp = array[j];
                     array[j] = array[j+1];
                     array[j+1] = temp;
                 }
@@ -32,7 +32,6 @@ public class BubbleSort {
         for (int i = 0; i < n; i++) {
             System.out.print(array[i]+ " ");
         }
-        System.out.println("");
+        System.out.println();
     }
-
 }
--- src/Sorting/bubbleSort/BubbleSortMain.java
+++ src/Sorting/bubbleSort/BubbleSortMain.java
@@ -3,7 +3,7 @@ package Sorting.bubbleSort;
 public class BubbleSortMain {
     public static void main(String[] args) {
         BubbleSort  bubsort = new BubbleSort();
-        int[] array ={17,23,10,14,2,1,8,0};
+        int[] array ={17,23,10,14,2,10,8,0};
         System.out.println("Array to be sorted ");
         bubsort.printArray(array);
 
