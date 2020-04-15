0fc438174cac667964b65ca4b9bba56635651d35
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-15 13:16:23 +0530

    create selectionSort() method

--- src/Sorting/selectionSort/SelectionSort.java
+++ src/Sorting/selectionSort/SelectionSort.java
@@ -1,4 +1,26 @@
 package Sorting.selectionSort;
 
 public class SelectionSort {
+    /**
+     *
+     * @param array
+     */
+    public static void selectionSort(int[] array){
+        int n = array.length;
+        for (int i = 0; i <n ; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < n; j++) {
+                if (array[j]<array[minIndex]){
+                    minIndex = j;
+                }
+                if (minIndex != i){
+                    int temp = array[minIndex];
+                    array[minIndex] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+
+
 }
