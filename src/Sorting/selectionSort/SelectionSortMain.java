6d42900bfc0126d0d47e1853303ebcb2f2ae3772
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-15 13:23:12 +0530

    create an Array in SelectionSortMain class

--- src/Sorting/selectionSort/SelectionSortMain.java
+++ src/Sorting/selectionSort/SelectionSortMain.java
@@ -1,4 +1,14 @@
 package Sorting.selectionSort;
 
 public class SelectionSortMain {
+    public static void main(String[] args) {
+        int[] array = { 12,34,20,13,45,39,54,81,20,12,20,10,5,6};
+
+        System.out.println("Array to be sorted");
+        SelectionSort.printArray(array);
+
+        System.out.println("Sorted Array....");
+        SelectionSort.selectionSort(array);
+        SelectionSort.printArray(array);
+    }
 }
