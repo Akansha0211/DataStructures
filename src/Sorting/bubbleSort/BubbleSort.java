b6f9d06a7fa9130c9a7fbfa8e812d3be32e32e61
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-15 11:42:33 +0530

    create bubbleSort() method

--- src/Sorting/bubbleSort/BubbleSort.java
+++ src/Sorting/bubbleSort/BubbleSort.java
@@ -1,4 +1,26 @@
 package Sorting.bubbleSort;
 
 public class BubbleSort {
+
+    /**
+     * This method wil sort the array according to BUBBLE SORT algorithm.
+     * In each iteration we will get the largest element to the last of the array
+     * we will not touch the last element anymore now
+     * Now loop till last element-1 and check similarly.
+     * Wgen we are left with the last  we need not do any comparision further and the array is sorted...
+     * @param array the array to be sorted.
+     */
+    public void bubbleSort(int[] array){
+        int n = array.length;
+        for (int i = 0; i <n-1 ; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (array[j]>array[j+1]){
+                    int temp = array[i];
+                    array[j] = array[j];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+
 }
