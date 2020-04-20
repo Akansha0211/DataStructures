ed3cd13e44d3334a66abc0fb0acb7dc47ef7dbb5
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-20 13:07:20 +0530

    create an integer array

--- src/Sorting/insertionSort/InsertionSortMain.java
+++ src/Sorting/insertionSort/InsertionSortMain.java
@@ -3,6 +3,7 @@ package Sorting.insertionSort;
 public class InsertionSortMain {
     public static void main(String[] args) {
         InsertionSort sortedArray = new InsertionSort();
-
+        int[] array = { 1,8,3,5,0,2,6,98,23,89};
+        //sortedArray.insertionSort(int[]array);
     }
 }
