6b5a7ad5a9a04db5146c63fccd94bb8b48a4c26f
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-20 13:08:05 +0530

    call insertionSort and printArray methods

--- src/Sorting/insertionSort/InsertionSortMain.java
+++ src/Sorting/insertionSort/InsertionSortMain.java
@@ -4,6 +4,7 @@ public class InsertionSortMain {
     public static void main(String[] args) {
         InsertionSort sortedArray = new InsertionSort();
         int[] array = { 1,8,3,5,0,2,6,98,23,89};
-        //sortedArray.insertionSort(int[]array);
+        sortedArray.insertionSort(array);
+        sortedArray.printArray(array);
     }
 }
