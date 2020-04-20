9e9f9a2d0395b53ae390bfa7328b4d589127ab83
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-20 13:05:57 +0530

    create object for InsertionSort class

--- src/Sorting/insertionSort/InsertionSortMain.java
+++ src/Sorting/insertionSort/InsertionSortMain.java
@@ -1,5 +1,8 @@
 package Sorting.insertionSort;
 
 public class InsertionSortMain {
+    public static void main(String[] args) {
+        InsertionSort sortedArray = new InsertionSort();
 
+    }
 }
