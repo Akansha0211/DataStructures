ee152855cf9c1c55f4dd58b5ff6ad8180bf8c2c4
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-04-21 09:38:56 +0530

    last commit for insertionSort package

--- src/Sorting/insertionSort/InsertionSortMain.java
+++ src/Sorting/insertionSort/InsertionSortMain.java
@@ -4,7 +4,13 @@ public class InsertionSortMain {
     public static void main(String[] args) {
         InsertionSort sortedArray = new InsertionSort();
         int[] array = { 1,8,3,5,0,2,6,98,23,89};
+        System.out.println("Unsorted Array");
+        sortedArray.printArray(array);
+        System.out.println("Unsorted Array");
+        long start = System.nanoTime();
         sortedArray.insertionSort(array);
+        long end = System.nanoTime();
+        System.out.println("Time to execute this algorithm = "+(end - start)+" nanoseconds");
         sortedArray.printArray(array);
     }
 }
