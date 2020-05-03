bfe02ed2d89f7593b3cb9b88a690721601f7b4b8
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-03 09:57:05 +0530

    changes in parameter of push() method of StackADT interface

--- src/Stacks/adt/StackADT.java
+++ src/Stacks/adt/StackADT.java
@@ -5,10 +5,10 @@ public interface StackADT<E> {
 
     /**
      * This method pushes the item onto the Top of Stack and eturns the item pushed(inserted).
-     * @param obj  : The object to be inserted
+     * @param data  : The data to be inserted
      * @return  The object inserted
      */
-    E push(E obj);
+    E push(E data);
 
     /**
      * This method returns the top of the stack without removing it
