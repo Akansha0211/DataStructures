98e89ad6f1975b74d68766b908aab1cd51e9b9bd
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-03 14:18:13 +0530

    Main class completed

--- src/Stacks/otherImple/Main.java
+++ src/Stacks/otherImple/Main.java
@@ -3,6 +3,12 @@ package Stacks.otherImple;
 public class Main {
     public static void main(String[] args) {
         StackASLinkedList stack = new StackASLinkedList();
-
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.pop();
+        stack.pop();
+        System.out.println(stack.peek());
     }
 }
