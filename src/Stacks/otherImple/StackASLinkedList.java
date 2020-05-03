903f348c7463d42963e0b6d8f7eb10a4e43cf9e4
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-03 13:40:05 +0530

    create a Node class in StackASLinkedList class

--- src/Stacks/otherImple/StackASLinkedList.java
+++ src/Stacks/otherImple/StackASLinkedList.java
@@ -1,4 +1,14 @@
 package Stacks.otherImple;
 
 public class StackASLinkedList {
+
+    private static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+
 }
