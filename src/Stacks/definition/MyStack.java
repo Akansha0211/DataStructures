fbb0d9abcc6389000ec10b8619feb60041593c74
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-03 10:05:33 +0530

    create Node class in MyStack class

--- src/Stacks/definition/MyStack.java
+++ src/Stacks/definition/MyStack.java
@@ -22,4 +22,22 @@ public class MyStack<E> implements StackADT<E>{
     public boolean isEmpty() {
         return false;
     }
+
+    private static class Node<E>{
+        private E data;
+        private Node<E> next;
+
+        public Node(E data, Node<E>next){
+            this.data = data;
+            this.next = next;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+    }
 }
