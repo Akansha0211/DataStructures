beb60073d77841deb0bd22f4ef60fc0135b42172
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-03 09:44:17 +0530

    method definition in  Stack adt for peek() method

--- src/Stacks/adt/StackADT.java
+++ src/Stacks/adt/StackADT.java
@@ -3,8 +3,20 @@ package Stacks.adt;
 public interface StackADT<E> {
 
 
+    /**
+     * This method pushes the item onto the Top of Stack and eturns the item pushed(inserted).
+     * @param obj  : The object to be inserted
+     * @return  The object inserted
+     */
     E push(E obj);
-//    E peek();
+
+    /**
+     * This method returns the top of the stack without removing it
+     * @post  the stack remains unchanged
+     * @return  The object at he top of the Stack
+     * @ throws NoSuchElementException if the Stack is Empty
+     */
+    E peek();
 //    E pop();
 //    boolean isEmpty();
 }
