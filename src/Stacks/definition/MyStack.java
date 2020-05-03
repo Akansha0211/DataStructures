effb909dadd6d358dd8ec8eedad1b1904f461213
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-03 09:55:00 +0530

    create MyStack class

---
+++ src/Stacks/definition/MyStack.java
@@ -0,0 +1,25 @@
+package Stacks.definition;
+
+import Stacks.adt.StackADT;
+
+public class MyStack<E> implements StackADT {
+    @Override
+    public Object push(Object obj) {
+        return null;
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public Object pop() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
