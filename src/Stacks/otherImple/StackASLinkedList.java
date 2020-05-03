261624f3a09c5d4e5f3076ce7540a7e14115894d
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-03 13:43:02 +0530

    create isEmpty() method

--- src/Stacks/otherImple/StackASLinkedList.java
+++ src/Stacks/otherImple/StackASLinkedList.java
@@ -1,13 +1,25 @@
 package Stacks.otherImple;
 
 public class StackASLinkedList {
-
+    Node head;
     private static class Node{
         int data;
         Node next;
         Node(int data){
             this.data = data;
         }
+    }
+
+    /**
+     * This method checks whether there the Stack is empty or not
+     * If the Stack is Empty it will return true otherwise false
+     * @return boolean value accordingly (as stated above)...
+     */
+    public boolean isEmpty(){
+        if (head == null){
+            return true;
+        }
+        else return false;
     }
 
 
