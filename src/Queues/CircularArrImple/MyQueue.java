f1a6a9124d2716c920e3f435332d998cf3ada03f
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-06 23:11:29 +0530

    create isFull() method

--- src/Queues/CircularArrImple/MyQueue.java
+++ src/Queues/CircularArrImple/MyQueue.java
@@ -14,4 +14,22 @@ public class MyQueue {
         arr = new int[size];
     }
 
+    /**
+     * This method will check if the array is full
+     * @return true if array is full otherwise return false...
+     */
+    public boolean isFull(){
+        if (rear+1 == front){
+            return true;
+        }
+        else if ((front == 0)&& (rear+1 ==size)){
+            return true;
+        }
+        else return false;
+    }
+
+//    public void enQueue(int value){
+//
+//    }
+
 }
