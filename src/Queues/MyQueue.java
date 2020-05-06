fb1a6d4ac04f25be2523b9c7de090c5885864e96
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-06 19:20:32 +0530

    create isEmpty() method

--- src/Queues/MyQueue.java
+++ src/Queues/MyQueue.java
@@ -10,4 +10,16 @@ public class MyQueue {
         rear = null;
         size = 0;
     }
+
+    /**
+     * This method will check whether the Queue is Empty
+     * @return true if Queue is empty or else return false
+     */
+    public  boolean isEmpty(){
+        boolean response = false;
+        if (size == 0){
+            response = true;
+        }
+         return response;
+    }
 }
