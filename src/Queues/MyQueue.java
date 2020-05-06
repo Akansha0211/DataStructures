58623f4bdecc4a2ee608a72a9ee24a36e508bc0b
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-06 19:50:55 +0530

    create dequeue() method

--- src/Queues/MyQueue.java
+++ src/Queues/MyQueue.java
@@ -5,7 +5,7 @@ public class MyQueue {
     private Node rear;  // works as like tail
     private int size;  // size of the queue
 
-    MyQueue(){
+    MyQueue() {
         front = null; // creating a blank Queue
         rear = null;
         size = 0;
@@ -13,14 +13,15 @@ public class MyQueue {
 
     /**
      * This method will check whether the Queue is Empty
+     *
      * @return true if Queue is empty or else return false
      */
-    public  boolean isEmpty(){
+    public boolean isEmpty() {
         boolean response = false;
-        if (size == 0){
+        if (size == 0) {
             response = true;
         }
-         return response;
+        return response;
     }
 
     /**
@@ -28,19 +29,40 @@ public class MyQueue {
      * Since it is a linear queue so adding of element is at one end
      * We are here considering the rear end is end where insertion is done.
      * So can't add at the other end .Only removal of elements i.e dequeue operation can be done from the front
+     *
      * @param nodeValue value which is added ....
      */
-    public void enQueue(int nodeValue){
+    public void enQueue(int nodeValue) {
         Node node = new Node(nodeValue); // create a node and add value to it...
         node.setNext(null);
-        if (rear == null){
+        if (rear == null) {
             front = rear = node;
             size++;
-        }
-        else {
+        } else {
             rear.setNext(node);
             rear = node;
             size++;
         }
+    }
+
+    /**
+     * This method will remove element from the Front of the Queue
+     * @return deleted element and if no element present in The Queue then return null...
+     */
+    public Node dequeue() {
+        Node tmpNode = null;
+        if (front != null) { // checking whether the Queue is empty...
+           if (front.getNext() != null){ // checking if the node to be deleted is the only node in the Queue...
+               tmpNode = new Node(front.getData());
+               front = front.getNext();
+               size--;
+           }
+           else {
+               tmpNode = new Node(front.getData());
+               front = rear = null;
+               size--;
+           }
+        }
+        return tmpNode;  // return null if the Queue is Empty...
     }
 }
