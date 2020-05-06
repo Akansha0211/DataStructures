72186a1d65a8051ef3ae8d337f0bfb428998b8a8
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-06 23:03:08 +0530

    create data-fields and constructor for MyQueue class (in CircularArrImple package)

--- src/Queues/CircularArrImple/MyQueue.java
+++ src/Queues/CircularArrImple/MyQueue.java
@@ -1,4 +1,17 @@
 package Queues.CircularArrImple;
 
+import java.util.Queue;
+
 public class MyQueue {
+    int front;
+    int rear;
+    int size;
+    int[]arr = new int[size];
+
+    MyQueue(int size){
+        this.size = size;
+        front = rear = -1;
+        arr = new int[size];
+    }
+
 }
