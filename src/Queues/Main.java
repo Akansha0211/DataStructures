932535b56fb52730bfeedb2467c5d297d0270dbb
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-06 22:18:33 +0530

    Main class completed

--- src/Queues/Main.java
+++ src/Queues/Main.java
@@ -1,4 +1,16 @@
 package Queues;
 
 public class Main {
+    public static void main(String[] args) {
+        MyQueue queue = new MyQueue();
+        queue.enQueue(1000);
+        System.out.println(queue.getSize());
+        System.out.println(queue.peek().getData()+ " is at the front");
+        queue.enQueue(10500);
+        queue.enQueue(25000);
+        queue.dequeue();
+        System.out.println(queue.peek().getData() + " is at the front");
+        System.out.println(queue.getSize() + " is the size of the Queue");
+    }
+
 }
