5d3146cac20a9017b05215dbdaafbfde689321e0
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-06 19:16:22 +0530

    create data fields and constructor for the MyQueue class

--- src/Queues/MyQueue.java
+++ src/Queues/MyQueue.java
@@ -1,4 +1,13 @@
 package Queues;
 
 public class MyQueue {
+    private Node front; // works as like head
+    private Node rear;  // works as like tail
+    private int size;  // size of the queue
+
+    MyQueue(){
+        front = null; // creating a blank Queue
+        rear = null;
+        size = 0;
+    }
 }
