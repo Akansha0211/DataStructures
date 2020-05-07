9f97c58e4dd391046df863ba3c157068249586bf
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-07 23:58:53 +0530

    Main class completed

--- src/Queues/CircularQLinkedList/Main.java
+++ src/Queues/CircularQLinkedList/Main.java
@@ -1,4 +1,16 @@
 package Queues.CircularQLinkedList;
 
 public class Main {
+    public static void main(String[] args) {
+        MyQueue queue = new MyQueue();
+        queue.enQueue(1000);
+        queue.enQueue(2000);
+        System.out.println(queue.getSize());
+        System.out.println(queue.peek().getData());
+//        Node delete = queue.dequeue();
+//        if (delete != null){
+//            System.out.println(delete.getData()+ " is deleted from the Queue");
+//        }
+        System.out.println(queue.dequeue().getData() + " is deleted from the Queue");
+    }
 }
