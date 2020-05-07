8f541c59e1bceabdbda1490d0a1255a8c7199ad1
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-07 23:27:16 +0530

    create enQueue() method

--- src/Queues/CircularQLinkedList/MyQueue.java
+++ src/Queues/CircularQLinkedList/MyQueue.java
@@ -23,4 +23,24 @@ public class MyQueue {
         return response;
     }
 
+    /**
+     * This method will insert an element in the Queue
+     * @param nodeValue which is to be inserted...
+     */
+    public void enQueue(int nodeValue){
+        Node node = new Node(nodeValue);
+        if(front == null){
+            //  only one node in the Queue which is self pointing  i.e front and rar points to same node
+            front = rear = node;
+            size++;
+        }
+        else {
+            rear = node;
+            // circular behaviour of Queue
+            rear.setNext(front.getNext());
+            size++;
+        }
+
+    }
+
 }
