87ba145b31ba6b21a6f1f30e1a8b3c16299f1d56
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-07 22:52:47 +0530

    create data fields and Constructor for the MyQueue class

--- src/Queues/CircularQLinkedList/MyQueue.java
+++ src/Queues/CircularQLinkedList/MyQueue.java
@@ -1,4 +1,14 @@
 package Queues.CircularQLinkedList;
 
 public class MyQueue {
+    private Node front;
+    private  Node rear;
+    private int size ;
+
+
+    MyQueue(){
+        front = rear = null;
+        size = 0;
+    }
+
 }
