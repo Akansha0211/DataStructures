cefd9fc0f2b8d79969eba259f5852b5f63e6c822
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-07 21:53:13 +0530

    call for dequeue() method in main() method

--- src/Queues/CircularArrImple/Main.java
+++ src/Queues/CircularArrImple/Main.java
@@ -1,6 +1,16 @@
 package Queues.CircularArrImple;
 
+
+//STILL SOME ISSUES WITH CIRCULAR QUEUE ARRAY IMPLE...
+
 public class Main {
-    MyQueue queue = new MyQueue();
+    public static void main(String[] args) {
+        MyQueue queue = new MyQueue();
+        queue.enQueue(1000);
+        queue.enQueue(1000);
+        System.out.println(queue.getSize());
+        queue.dequeue();
+    }
+
 
 }
