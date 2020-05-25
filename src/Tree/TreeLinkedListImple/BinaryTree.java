c4bbc8434f8cb187de48da953a212dbdd6c6d6f4
Akansha0211 <akansha.saxena_cs18@gla.ac.in>
2020-05-25 19:25:05 +0530

    create insert() method

--- src/Tree/TreeLinkedListImple/BinaryTree.java
+++ src/Tree/TreeLinkedListImple/BinaryTree.java
@@ -12,4 +12,32 @@ public class BinaryTree {
             right = null;
         }
     }
+
+    /**
+     * This method will add a node in orderly manner  to keep them sorted
+     *If we want ot add a node , rules are:
+     * If the new node's value is lower than the current node's value go to the left child,
+     * If new node's value is greater than the current node's value go to the right child,
+     * When the current node is null we have reached a leaf node , we insert the new node in that position.
+     * @param node is the current node.
+     * @param value node's value which is to be added
+     */
+    public void insert(Node node, int value){
+        if (value<node.value){
+            if (node.left != null){
+                insert(node.left,value);
+            }else {
+                System.out.println("Inserted "+ value+ " to the left of "+ node.value);
+                node.left = new Node(value);
+            }
+        }
+        else if (value> node.value){
+            if (node.right != null){
+                insert(node.right,value);
+            }else {
+                System.out.println("Inserted "+ value+" to the right of "+node.value);
+                node.right = new Node(value);
+            }
+        }
+    }
 }
