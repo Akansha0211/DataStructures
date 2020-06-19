5b326a9f48068fc566da62049954d4fa3626adae
Akansha0211 <sanjaysxn25@gmail.com>
2020-06-19 22:55:39 +0530

    create BT class object

--- src/Trees/BinaryTree/Main.java
+++ src/Trees/BinaryTree/Main.java
@@ -1,7 +1,30 @@
 package Trees.BinaryTree;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+        BT binaryTree = new BT();
+        System.out.println("Enter your choice\n" +
+                "Press 1 to insert an element\n" +
+                "Press 2 to search an element\n" +
+                "Press 3 to check if empty");
+        int choice = scanner.nextInt();
+        switch (choice){
+            case 1:
+                System.out.println("Enter an integer element that you want to insert");
+                binaryTree.insert(scanner.nextInt());
+                break;
+            case 2:
+                System.out.println("Enter an integer element that you want to search");
+                binaryTree.search(scanner.nextInt());
+                break;
+            default:
+                System.out.println("Wrong Entry");
+                break;
+
+        }
 
     }
 }
