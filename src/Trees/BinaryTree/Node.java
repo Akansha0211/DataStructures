460b169353666a05dff08c2a582c01161e7c31cb
Akansha0211 <sanjaysxn25@gmail.com>
2020-06-19 18:18:03 +0530

    Node class complete

--- src/Trees/BinaryTree/Node.java
+++ src/Trees/BinaryTree/Node.java
@@ -1,4 +1,36 @@
 package Trees.BinaryTree;
 
 public class Node {
+    int data;
+    Node left;
+    Node right;
+    public Node(int value){
+        left = null;
+        right = null;
+        this.data = value;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
 }
