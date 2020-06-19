63867df780eaf0175ac5b928c643de539b1e3bd3
Akansha0211 <sanjaysxn25@gmail.com>
2020-06-19 17:50:11 +0530

    BT class created

---
+++ src/Trees/BT.java
@@ -0,0 +1,19 @@
+package Trees;
+
+/**
+ * A binary tree is a non-linear DS where each node can have atmost two children.
+ *                2
+ *             /     \
+ *            7       5
+ *           / \       \
+ *          2   6       9
+ *             /  \     /
+ *            5   11   4
+ *
+ *    Leaf node - have 0 children.
+ *    Now to search any node you will have to search entire tree in worst case scenario (Time complexity = O(n))
+ *    As there is no ordering.
+ *    Insertion of node is done in level order.
+ */
+public class BT {
+}
